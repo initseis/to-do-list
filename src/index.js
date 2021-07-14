2a0dad1b8a8c096b03f34129912a803e741acff3
Kevin <kevinsevinche@gmail.com>
2021-07-14 16:17:30 -0500

    Save check changes in local storage

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ import { Status } from "./status.js";
 const list = [
   {
     index: 2,
-    description: "2 Wash the dishes",
+    description: "Wash the dishes",
     completed: false,
   },
   {
     index: 1,
-    description: "1 Complete To Do list project",
+    description: "Complete To Do list project",
     completed: false,
   },
   {
     index: 4,
-    description: "4 Go out the dog",
+    description: "Go out the dog",
     completed: false,
   },
   {
     index: 3,
-    description: "3 Go to the gym",
+    description: "Go to the gym",
     completed: false,
   },
 ];
